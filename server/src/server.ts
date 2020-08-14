c0e49f65fc139ffbdf527e88fa902786ed269e46
CesarAugusto <cesaraugusto.ls@hotmail.com>
2020-08-13 22:48:47 -0300

    post create class

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,11 @@
-console.log("hello 2")
+import express from 'express'
+import routes from './routes';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+
+// port server
+app.listen(3333);
