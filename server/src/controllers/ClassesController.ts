e4226acb8842dc1803bfb851017460f2e0bfc83d
CesarAugusto <cesaraugusto.ls@hotmail.com>
2020-08-16 17:06:11 -0300

    create/list connections

--- server/src/controllers/ClassesController.ts
+++ server/src/controllers/ClassesController.ts
@@ -1,4 +1,4 @@
-import { Request, Response, response } from 'express'
+import { Request, Response } from 'express'
 
 import db from '../database/connection';
 import convertHourToMinutes from '../utils/convertHourToMinutes';
@@ -89,6 +89,8 @@ export default class ClassesController {
 
             return response.status(201).send();
         } catch (err) {
+            console.log(err)
+            
             await trx.rollback();
             return response.status(400).json({
                 error: 'Unexpected error while creating new class'
