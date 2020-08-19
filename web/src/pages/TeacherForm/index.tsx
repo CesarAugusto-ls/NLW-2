61d331cc8908a0da62774edd810820d7a9b31b3f
CesarAugusto <cesaraugusto.ls@hotmail.com>
2020-08-18 21:00:27 -0300

    teacher list

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
 
+import './styles.css'
+import Input from '../../components/Input';
+
+import warningIcon from '../../assets/images/icons/warning.svg'
+
 function TeacherForm() {
     return (
         <div id="page-teacher-form" className="container">
@@ -9,7 +14,31 @@ function TeacherForm() {
                 description="O primeiro passo é preencher esse formulário de inscrição"
             />
 
-            
+            <main>
+                <fieldset>
+                    <legend>Seus dados</legend>
+                    <Input name="name" label="Nome completo" />
+                    <Input name="avatar" label="Avatar" />
+                    <Input name="whatsapp" label="WhatsApp" />
+                </fieldset>
+
+                <fieldset>
+                    <legend>Sobre a aula</legend>
+                    <Input name="msubject" label="Matéria" />
+                    <Input name="cost" label="Custo da sua hora por aula" />
+                </fieldset>
+
+                <footer>
+                    <p>
+                        <img src={warningIcon} alt="Aviso importante" />
+                        Importante! <br />
+                        Preencha todos os dados
+                    </p>
+                    <button type="button">
+                        Salvar cadastro
+                    </button>
+                </footer>
+            </main>
         </div>
     )
 }
