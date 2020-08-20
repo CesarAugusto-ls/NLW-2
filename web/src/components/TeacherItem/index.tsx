e9dd61b23c8b97b15f9552faa5039388c98e0664
CesarAugusto <cesaraugusto.ls@hotmail.com>
2020-08-19 22:12:26 -0300

    get classes

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -4,30 +4,40 @@ import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css'
 
-const TeacherItem: React.FC = () => {
+
+interface TeacherItemProps {
+    teacher: {
+        id: number,
+        avatar: string,
+        bio: string,
+        cost: number,
+        name: string,
+        subject: string,
+        user_id: number,
+        whatsapp: string,
+    }
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars3.githubusercontent.com/u/29068826?s=460&u=488b529fd3aaf3dd947c72d97f24b18a73b5b388&v=4" alt="Cesar Augusto" />
+                <img src={teacher.avatar} alt={teacher.name} />
                 <div>
-                    <strong>Cesar Augusto</strong>
-                    <span>Botânica</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
-            <p>
-                Estudo da fisiologia, morfologia, ecologia, evolução, anatomia, classificação, doenças, distribuição, dentre outros aspectos das plantas.
-                        <br /><br />
-                        Essa ciência foi reconhecida como tal em 1979, juntamente com os cursos de Biologia.
-                    </p>
+            <p>{teacher.bio}</p>
             <footer>
                 <p>
                     Preço/hora
-                            <strong>R$ 162,00</strong>
+                            <strong>{`R$ ${teacher.cost}`}</strong>
                 </p>
-                <button type="button">
+                <a href={`https://wa.me/${teacher.whatsapp}`}>
                     <img src={whatsappIcon} alt="whatsapp" />
-                            Entrar em contato
-                        </button>
+                        Entrar em contato
+                </a>
             </footer>
         </article>
     )
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,20 +1,43 @@
-import React from 'react';
+import React, { FormEvent } from 'react';
 
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
 import Input from '../../components/Input';
 import Select from '../../components/Select';
 
+import api from '../../services/api';
 import './styles.css'
 
 function TeacherList() {
+    const [teachers, setTeachers] = React.useState([])
+
+    const [subject, setSubject] = React.useState('');
+    const [week_day, setWeekDay] = React.useState('');
+    const [time, setTime] = React.useState('');
+
+    async function searchTeacher(e: FormEvent) {
+        e.preventDefault();
+
+        const response = await api.get('/classes', {
+            params: {
+                subject,
+                week_day,
+                time
+            }
+        })
+
+        setTeachers(response.data);
+    }
+
     return (
         <div id="page-teacher-list" className="container">
             <PageHeader title="Estes são os proffys disponíveis">
-                <form id="search-teachers">
+                <form id="search-teachers" onSubmit={searchTeacher}>
                     <Select
                         name="subject"
                         label="Matéria"
+                        value={subject}
+                        onChange={(e) => setSubject(e.target.value)}
                         options={[
                             { value: 'Artes', label: 'Artes' },
                             { value: 'Biologia', label: 'Biologia' },
@@ -27,6 +50,8 @@ function TeacherList() {
                     <Select
                         name="week_day"
                         label="Dia da semana"
+                        value={week_day}
+                        onChange={(e) => setWeekDay(e.target.value)}
                         options={[
                             { value: '0', label: 'Domingo' },
                             { value: '1', label: 'Segunda-feira' },
@@ -37,12 +62,24 @@ function TeacherList() {
                             { value: '6', label: 'Sábado' },
                         ]}
                     />
-                    <Input type="time" name="time" label="Hora" />
+                    <Input
+                        type="time"
+                        name="time"
+                        label="Hora"
+                        value={time}
+                        onChange={(e) => setTime(e.target.value)}
+                    />
+
+                    <button type='submit' onClick={searchTeacher}>
+                        buscar
+                    </button>
                 </form>
             </PageHeader>
 
             <main>
-                <TeacherItem />
+                {teachers.map((teacher, index) => {
+                    return <TeacherItem key={index} teacher={teacher} />
+                })}
             </main>
         </div>
     )
